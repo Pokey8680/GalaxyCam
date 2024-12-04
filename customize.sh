7fa1cc509e7a73adb9d05811820d6afce31437aa
Pokey8680 <zidan.morgan2@gmail.com>
2024-12-03 20:01:31 -0500

    Updated customize.sh to not create the specific folders & fail

--- customize.sh
+++ customize.sh
@@ -5,34 +5,15 @@
 # Define the target directory for the Samsung Camera
 TARGET_DIR="/system/priv-app/SamsungCamera"
 
-# Display a message to the user
-ui_print "Samsung Camera Magisk Module Installation"
-ui_print "This module will install the Samsung Camera"
-ui_print "app as a system app in the following location:"
-ui_print "$TARGET_DIR"
+# Display message to the user
+ui_print " "
+ui_print "Putting app files in $TARGET_DIR"
 
-# Check if the directory already exists
-if [ -d "$TARGET_DIR" ]; then
-    ui_print "Directory $TARGET_DIR already exists. Proceeding with the installation."
-else
-    ui_print "Creating directory $TARGET_DIR."
-    mkdir -p "$TARGET_DIR"
-fi
+# Copy apk files to /system/priv-app/SamsungCamera (Magisk does this automatically)
 
-# Copy the Samsung Camera APK and associated files to the system partition
-cp -R $MODPATH/system/priv-app/SamsungCamera/* $TARGET_DIR/
+# Set proper permissions
+chmod -R 755 $TARGET_DIR
 
-# Verify that the files were copied
-if [ -f "$TARGET_DIR/SamsungCamera.apk" ]; then
-    ui_print "Samsung Camera APK placed successfully."
-else
-    ui_print "Error: Samsung Camera APK wasn't installed successfully!"
-    exit 1
-fi
-
-# Ensure proper file permissions
-chmod 755 "$TARGET_DIR/SamsungCamera.apk"
-chmod 755 "$TARGET_DIR/oat/arm64/*"
-
+ui_print " "
 ui_print "Installation complete!"
-ui_print "You can now reboot your device to apply the changes."
+ui_print "Please reboot your device to apply the changes."
